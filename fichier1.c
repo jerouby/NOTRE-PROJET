7e70778706401863ba570d5389b25b89b59184db
GOUEZ Virgil <vigouez@etud.rcisima.isima.fr>
2018-02-15 16:42:03 +0100

    kjdwvhdxjdsgv

--- fichier1.c
+++ fichier1.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main() {
 	printf("Coucou\n");
+ceci est un message crée de toute pièce par virgil gouez
 	return 0;
 }
+
+
+
+
+
