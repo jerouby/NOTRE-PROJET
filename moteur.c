98cbd07fbc078d015b55925fc232a6b4a0f30392
GOUEZ Virgil <vigouez@etud.rcisima.isima.fr>
2018-02-15 17:28:34 +0100

    grjhgjhdgd

--- moteur.c
+++ moteur.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #define TAILLE_X 60
-#define TAILLE_Y 10
+#define TAILLE_Y 20
 
 void creerZone(int t[TAILLE_X][TAILLE_Y]){
 	int i,j=0;
 	for (i=0;i<TAILLE_X;i++){
 		for(j=0;j<TAILLE_Y;j++){
-			if(i==0 || j==0 || i == TAILLE_X -1 || j == TAILLE_Y -1 || (i==5 && (j==10 || j==11 || j==12))){	//Insérer les murs ici
+			if(i==0 || j==0 || i == TAILLE_X -1 || j == TAILLE_Y -1 || rand()%8==6){	//Insérer les murs ici
 				t[i][j]=1;
 			}		
 			else if ((i==2 && j ==2)) {	//Insérer les coordonnées des monstres ici
@@ -77,34 +77,37 @@ void affichage(int tab[TAILLE_X][TAILLE_Y]){
 int main () {
 	int t[TAILLE_X][TAILLE_Y];
 	int jeu_fini = 0;			//variable =1 si le joueur a gagné ou 2 si il est mort
-	int i,j = 0;
+	int i,j, k = 0;
 	int x,y = 2;		//Le monstre se situe 
-	int compteur = 0;
-
+	int monstres[10][2] = {0};	//un tableau qui contiendra la liste de coordonnées des monstres, on considère qu'il y en aura au plus 10
+	srand(time(NULL));
+	int nbMonstre = 0;
 	creerZone(t);	//Initialisation de la zone
-
 
 	while(jeu_fini == 0){
 		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		affichage(t[TAILLE_X][TAILLE_Y]);	//affichage de la zone
-		monstre(t[TAILLE_X][TAILLE_Y],x,y);	//Déplacement du monstre
+		affichage(t);	//affichage de la zone
+		monstre(t,x,y);	//Déplacement du monstre
 
 		for (i=0;i<TAILLE_X;i++){		//recherche de la nouvelle position du monstre
 			for(j=0;j<TAILLE_Y;j++){
 				if(t[i][j]==2){
-					x=i;
-					y=j;
+					monstres[nbMonstre][0]=i;
+					monstres[nbMonstre][1]=j;
+					nbMonstre++;		
 				}
 			}
 		}
+		while(monstres[k][0] != 0){ //tant qu'il y a des monstres à faire bouger
+			monstre(t,monstres[k][0],monstres[k][1]);
+			k++;		
+		}
 
 		sleep(1);	//Temps d'affichage
-		compteur++;
-		if(compteur=10){
-			jeu_fini = 1;
-		}
-	}
-	printf("\n Bien joué ! \n");
 
-	return 0;
+	}
+
+printf("\n Bien joué ! \n");
+
+return 0;
 }
